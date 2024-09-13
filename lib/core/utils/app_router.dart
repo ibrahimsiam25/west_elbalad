8022cf4ba4982a1eb72bc9048b0cf144f4ae5f52
ibrahimsiam25 <ebrahimfathy2822@gmail.com>
2024-09-13 19:59:39 +0300

    ref

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -30,7 +30,7 @@ abstract class AppRouter {
   static final router = GoRouter(
     routes: [
       GoRoute(
-        path: "/sp",
+        path: "/",
         builder: (context, state) => const SplashView(),
       ),
       GoRoute(
@@ -70,7 +70,8 @@ abstract class AppRouter {
         builder: (context, state) => const SignUpSuccessfully(),
       ),
       GoRoute(
-        path: "/",
+        
+        path: kAdminView,
         builder: (context, state) => const AdminView(),
       ),
       GoRoute(
