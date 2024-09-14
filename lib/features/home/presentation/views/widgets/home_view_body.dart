3ae3783e70888f5c6b9d7b1e2971d6df694d2aa4
itiswd <itiswdev97@gmail.com>
2024-09-14 23:34:49 +0300

    Refactor filter list padding to improve UI consistency.

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:west_elbalad/core/constants/app_consts.dart';
 import 'package:west_elbalad/features/home/domian/entites/phone_entites.dart';
 import 'package:west_elbalad/features/home/presentation/manager/phones_filter/filter_cubit.dart';
 import 'package:west_elbalad/features/home/presentation/views/widgets/filter_list.dart';
@@ -51,11 +52,10 @@ class HomeViewBody extends StatelessWidget {
                             .toList(),
                         //For alighnment
                         Container(
-                          padding: EdgeInsets.all(16.0),
-                          margin: EdgeInsets.only(bottom: 16.0.h),
+                          padding: EdgeInsets.symmetric(
+                              horizontal: kHorizontalPadding),
                           child: SizedBox(
                             width: 128.0.w,
-                            height: 128.0.h,
                           ),
                         )
                       ],
