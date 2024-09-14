b0e3b6143a5bd1ba7ee27e97232f6380579e1b30
itiswd <itiswdev97@gmail.com>
2024-09-14 11:34:19 +0300

    Splash done.

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:video_player/video_player.dart';
+import 'package:west_elbalad/core/constants/app_colors.dart';
 import '../../../../../core/constants/app_consts.dart';
 import 'package:west_elbalad/core/utils/app_router.dart';
 import '../../../../../core/service/shared_preferences_singleton.dart';
@@ -36,13 +37,23 @@ class _SplashViewBodyState extends State<SplashViewBody> {
   @override
   void initState() {
     super.initState();
-    _controller = VideoPlayerController.asset('assets/images/splash.mp4');
-    _controller.addListener(() {
-      setState(() {});
-    });
-    _controller.setLooping(true);
-    _controller.initialize().then((_) => setState(() {}));
+    _controller = VideoPlayerController.asset(
+      'assets/images/splash.mp4',
+    )
+      ..initialize().then((_) {
+        setState(() {});
+      })
+      ..addListener(() {
+        if (_controller.value.isPlaying) {
+          setState(() {});
+        } else if (_controller.value.position.inMilliseconds -
+                _controller.value.duration.inMilliseconds <
+            1) {
+          setState(() {});
+        } else {}
+      });
     _controller.play();
+    _controller.setLooping(true);
     executeNavigation();
   }
 
@@ -58,7 +69,18 @@ class _SplashViewBodyState extends State<SplashViewBody> {
       child: _controller.value.isInitialized
           ? AspectRatio(
               aspectRatio: _controller.value.aspectRatio,
-              child: VideoPlayer(_controller),
+              child: Stack(children: [
+                VideoPlayer(_controller),
+                Column(
+                  children: [
+                    Spacer(),
+                    Container(
+                      height: 6.0,
+                      color: AppColors.white,
+                    ),
+                  ],
+                )
+              ]),
             )
           : const SizedBox(),
     );
