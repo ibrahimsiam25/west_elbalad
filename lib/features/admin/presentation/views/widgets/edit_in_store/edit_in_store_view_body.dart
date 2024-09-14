1165dbb2eccb419685c28b67b6694c9ea509d79e
ibrahimsiam25 <ebrahimfathy2822@gmail.com>
2024-09-14 20:05:37 +0300

    d

--- lib/features/admin/presentation/views/widgets/edit_in_store/edit_in_store_view_body.dart
+++ lib/features/admin/presentation/views/widgets/edit_in_store/edit_in_store_view_body.dart
@@ -5,6 +5,8 @@ class EditInStoreViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Column(
+      children: [],
+    );
   }
 }
